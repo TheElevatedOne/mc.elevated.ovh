6d2a9cdf957b85bd54d1acc18169d08e0fc094f5
Adam Mladý <chickpop24@gmail.com>
2024-11-20 00:35:27 +0100

    made them work onload

--- js/panorama.js
+++ js/panorama.js
@@ -1,13 +1,10 @@
+const panoramas = [
+    "panoramas/01.jpg",
+    "panoramas/02.jpg",
+    "panoramas/03.jpg"
+];
+
 function rPano() {
-    const panoramas = [
-        "./panoramas/01.jpg",
-        "./panoramas/02.jpg",
-        "./panoramas/03.jpg"
-    ];
-
     let random = Math.floor(Math.random() * panoramas.length);
-
-    document.getElementById("panorama").src = panoramas[random];
+    $("#panorama").attr("src", panoramas[random])
 }
-
-setTimeout(rPano, 2000)
